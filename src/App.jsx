73b03de0174838eb7b00ddfb33df3af39ee00623
Phadon Makara <phadonmakara@gmail.com>
2025-06-19 23:50:09 +0700

    added Toastify

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,17 @@ import { useState } from 'react'
 import Form from './Form'
 import Items from './Items'
 import { nanoid } from 'nanoid'
+import { ToastContainer, toast } from 'react-toastify'
 
-const getLocalStorage = () => {
-  let list = localStorage.getItem('list')
-  if (list) {
-    list = JSON.parse(localStorage.getItem('list'))
-  } else {
-    list = []
-  }
-  return list
-}
+// const getLocalStorage = () => {
+//   let list = localStorage.getItem('list')
+//   if (list) {
+//     list = JSON.parse(localStorage.getItem('list'))
+//   } else {
+//     list = []
+//   }
+//   return list
+// }
 
 const setLocalStorage = (items) => {
   localStorage.setItem('list', JSON.stringify(items))
@@ -30,16 +31,19 @@ const App = () => {
     const newItems = [...items, newItem]
     setItems(newItems)
     setLocalStorage(newItems)
+    toast.success('item added to the list')
   }
   const deleteItem = (toDelete) => {
     const newItems = items.filter((item) => item !== toDelete)
     setItems(newItems)
     setLocalStorage(newItems)
+    toast.success('item deleted from the list')
   }
   const editItem = (editItemId) => {
     const newItems = items.map((item) => {
       if (item.id === editItemId) {
-        item.completed = !item.completed
+        const newItem = { ...item, completed: !item.completed }
+        return newItem
       }
       return item
     })
@@ -49,6 +53,7 @@ const App = () => {
 
   return (
     <section className='section-center'>
+      <ToastContainer position='top-center' />
       <Form addItem={addItem} />
       <Items items={items} deleteItem={deleteItem} editItem={editItem} />
     </section>
